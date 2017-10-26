541101000e29fb8493f4c29ae4db2948f3420999
Nikola Uzelac <nikola@uzelac.io>
2017-10-25 20:42:54 -0700

    Drag animation done through interpolation.

--- src/Deck.js
+++ src/Deck.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
     constructor(props) {
         super(props);
 
         const position = new Animated.ValueXY();
+
         const panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (event, gesture) => {
@@ -17,13 +20,27 @@ class Deck extends Component {
         this.state = { panResponder, position };
     }
 
+    getCardStyle() {
+        const { position } = this.state;
+
+        const rotate = position.x.interpolate({
+           inputRange: [-SCREEN_WIDTH * 2, 0, SCREEN_WIDTH * 2],
+           outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            ...this.state.position.getLayout(),
+            transform: [{ rotate }]
+        };
+    }
+
     renderCards() {
         return this.props.data.map((item, index) => {
             if (index === 0) {
                 return (
                     <Animated.View
                         key = {item.id}
-                        style = {this.state.position.getLayout()}
+                        style = {this.getCardStyle()}
                         {...this.state.panResponder.panHandlers}
                     >
                         {this.props.renderCard(item)}
